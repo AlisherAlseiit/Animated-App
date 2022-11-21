acf8c011eed8474c9c894ae30c914b50f55692bc
Alisher <megawin51@gmail.com>
2022-11-21 13:54:23 +0600

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Alish Alseiit on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
